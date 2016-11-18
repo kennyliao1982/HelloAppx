c78e0d755ad4404a468137f0cbfe946b2b8088e9
FRank <tofranklu@hotmail.com>
2016-11-18 20:09:21 +0800

    dog

--- src/hello/HelloWorld.java
+++ src/hello/HelloWorld.java
@@ -3,11 +3,9 @@ package hello;
 public class HelloWorld {
 
   public static void main(String[] args) {
-<<<<<<< HEAD
-    System.out.println("Hello dog!");
-=======
-    System.out.println("Hello cat!");
->>>>>>> branch 'master' of https://github.com/kennyliao1982/HelloAppx.git
+
+    System.out.println("Hello dog cat!");
+
   }
 
 }
