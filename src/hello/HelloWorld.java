e031e701aae9252a54c18625c751fb5cbc8a559b
amberchen <ya810720@gmail.com>
2016-11-18 20:07:12 +0800

    cat2

--- src/hello/HelloWorld.java
+++ src/hello/HelloWorld.java
@@ -3,7 +3,14 @@ package hello;
 public class HelloWorld {
 
   public static void main(String[] args) {
-    System.out.println("Hello cat!");
+    
+    
+	  
+	  
+	  
+	  
+	  
+	  System.out.println("Hello cat2!");
   }
 
 }
